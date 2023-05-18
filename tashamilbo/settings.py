50d289f45fa2b4dcb61853c57b1b42ff3beb46e0
Nicolae Caldarasan <nicolaecaldarasan1983@gmail.com>
2023-05-18 13:25:32 +0100

    Account activation

--- tashamilbo/settings.py
+++ tashamilbo/settings.py
@@ -142,3 +142,12 @@ from django.contrib.messages import constants as messages
 MESSAGE_TAGS = {
     messages.ERROR: "danger",
 }
+
+
+# SMTP CONFIGURATION
+# EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = '587'
+EMAIL_USE_TLS = 'True'
+
+EMAIL_HOST_USER = 'tashamilbo@gmail.com'
