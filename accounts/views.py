b9d8b8f8e43285d4427ad5a25f227708b14b9fd1
Nicolae Caldarasan <nicolaecaldarasan1983@gmail.com>
2023-05-23 12:23:41 +0100

    Making Orde Model, Product Model, and Payment Model

--- accounts/views.py
+++ accounts/views.py
@@ -14,6 +14,7 @@ from django.contrib.auth.tokens import default_token_generator
 from django.core.mail import EmailMessage
 from carts.views import _cart_id
 from carts.models import Cart, CartItem
+import requests
 # Create your views here.
 
 def register(request):
@@ -103,7 +104,15 @@ def login(request):
 
             auth.login(request, user)
             messages.success(request, "You are now logged in.")
-            return redirect('dashboard')
+            url = request.META.get('HTTP_REFERER')
+            try:
+                query = requests.utils.urlparse(url).query
+                params = dict(x.split('=') for x in query.split('&'))
+                if 'next' in params:
+                    nextPage = params['next']
+                    return redirect(nextPage)     
+            except:
+                return redirect('dashboard')
         else:
             messages.error(request, "Invalid login credentials")
             return redirect('login')
