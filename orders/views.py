de2d0a186f3ec21a0990126e695ede82a17c89f4
Nicolae Caldarasan <nicolaecaldarasan1983@gmail.com>
2023-06-10 13:38:59 +0100

    Generate invoice on order completion

--- orders/views.py
+++ orders/views.py
@@ -8,6 +8,7 @@ import json
 from store.models import Product
 from django.core.mail import EmailMessage
 from django.template.loader import render_to_string
+
 # Create your views here.
 
 def payments(request):
@@ -67,8 +68,6 @@ def payments(request):
         'order_number': order.order_number,
         'transID': payment.payment_id,
     }
-
-
     return JsonResponse(data)
 
 
@@ -135,8 +134,30 @@ def place_order(request, total=0, quantity=0):
 def order_complete(request):
     order_number = request.GET.get('order_number')
     transID = request.GET.get('payment_id')
+
+    try:
+        order = Order.objects.get(order_number=order_number, is_ordered=True)
+        ordered_products = OrderProduct.objects.filter(order_id=order.id)
+        
+        subtotal = 0
+        for i in ordered_products:
+            subtotal += i.product_price * i.quantity
+        payment = Payment.objects.get(payment_id=transID)
+
+        context = {
+            'order': order,
+            'ordered_products': ordered_products,
+            'order_number': order.order_number,
+            'transID': payment.payment_id,
+            'payment': payment,
+            'subtotal': subtotal,
+        }
+        return render(request, 'orders/order_complete.html', context)
+    except (Payment.DoesNotExist, Order.DoesNotExist):
+        return redirect('home')
+
     
-    return render(request, 'orders/order_complete.html')
+    
     
     
     
